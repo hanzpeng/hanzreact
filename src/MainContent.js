fe0de3c20588beef6a8b528d15a87fa7ae603363
Hanz Zhang <hanzzhan@microsoft.com>
2022-05-03 18:38:05 -0700

    add Header, Footer, MainContent

---
+++ src/MainContent.js
@@ -0,0 +1,23 @@
+export default function MainContent() {
+    return (
+      <div>
+        <h2>Welcome to Hanz React Page</h2>
+        <ol>
+          <li>test111</li>
+          <li>test222</li>
+          <li>test333</li>
+        </ol>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    )
+  }
