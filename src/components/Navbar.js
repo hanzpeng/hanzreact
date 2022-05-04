e1c54d88606baaa37a0a7858bedd082c659b7cd8
Hanz Zhang <hanzzhan@microsoft.com>
2022-05-04 09:43:43 -0700

    add component folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src={logo} className="App-logo" alt="logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
