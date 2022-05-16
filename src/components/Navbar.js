4a1bc29251c866216f9addbcce8ec4182bd44862
Hanz Zhang <hanzzhan@microsoft.com>
2022-05-15 21:31:00 -0700

    change a to Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import logo from '../images/logo.svg';
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
     return (
@@ -6,9 +7,9 @@ export default function Navbar() {
             <nav className="nav">
                 <img src={logo} className="App-logo" alt="logo" />
                 <ul className="nav-items">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/githubcards">Github Hards</a></li>
-                    <li><a href="/starmatch">StartMatch</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/githubcards">Github Hards</Link></li>
+                    <li><Link to="/starmatch">StartMatch</Link></li>
                 </ul>
             </nav>
         </header>
