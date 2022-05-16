9667d057bc4bacf9f31cffd483db1020aa7692f5
Hanz Zhang <hanzzhan@microsoft.com>
2022-05-15 21:10:42 -0700

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import './App.css';
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import MainContent from './components/MainContent';
-import GithubCards from './components/GithubCards';
-import StarMatch from './components/StarMatch';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+import GithubCards from "./components/GithubCards";
+import StarMatch from "./components/StarMatch";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <MainContent></MainContent>
-      <GithubCards title="Add Github Cards"></GithubCards>
-      <StarMatch />
-      <Footer></Footer>
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<MainContent/>} />
+          <Route exact path="/github" element={<GithubCards/>} />
+          <Route exact path="/starmatch" element={<StarMatch/>} />
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
+
+function NotFound(){
+  return (
+    <div>
+      page not found
+    </div>
+  )
+};
 
 export default App;
