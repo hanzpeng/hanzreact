8c58c3a2e8b6d560d9ec86eda7f1f0ca014638bf
Hanz Zhang <hanzzhan@microsoft.com>
2022-05-17 17:03:31 -0700

    remove class component

--- src/components/GithubCards.js
+++ src/components/GithubCards.js
@@ -11,20 +11,6 @@ const CardList = (props) => (
 	</div>
 );
 
-class CardNotUsed extends React.Component {
-	render() {
-  	const profile = this.props;
-  	return (
-    	<div className="github-profile">
-    	  <img src={profile.avatar_url} />
-        <div className="info">
-          <div className="name">{profile.name}</div>
-          <div className="company">{profile.company}</div>
-        </div>
-    	</div>
-    );
-  }
-}
 
 const Card = (profile) => (
   <div className="github-profile">
@@ -37,30 +23,6 @@ const Card = (profile) => (
   </div>
 );
 
-class FormNotUsed extends React.Component {
-	state = { userName: '' };
-	handleSubmit = async (event) => {
-  	event.preventDefault();
-    //const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
-    const resp = await fetch(`https://api.github.com/users/${this.state.userName}`);
-    this.props.onSubmit(resp.data);
-    this.setState({ userName: '' });
-  };
-	render() {
-  	return (
-    	<form onSubmit={this.handleSubmit}>
-    	  <input 
-          type="text" 
-          value={this.state.userName}
-          onChange={event => this.setState({ userName: event.target.value })}
-          placeholder="GitHub username" 
-          required 
-        />
-        <button>Add card</button>
-    	</form>
-    );
-  }
-}
 
 const Form = (props) => {
   const [userName, setUserName] = useState('');
@@ -84,25 +46,6 @@ const Form = (props) => {
   );
 }
 
-class GithubCardsNotUsed extends React.Component {
-  state = {
-    profiles: [],
-  };
-  addNewProfile = (profileData) => {
-  	this.setState(prevState => ({
-    	profiles: [...prevState.profiles, profileData],
-    }));
-  };
-	render() {
-  	return (
-    	<div>
-    	  <div className="header">{this.props.title}</div>
-        <Form onSubmit={this.addNewProfile} />
-        <CardList profiles={this.state.profiles} />
-    	</div>
-    );
-  }	
-}
 
 const GithubCards = (props) => {
   const {id} = useParams;
