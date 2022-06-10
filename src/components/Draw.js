164dc0c76bfc479d7dcf7e08552575604d094414
Hanz Zhang <hanzzhan@microsoft.com>
2022-06-09 22:47:00 -0700

    update Draw

--- src/components/Draw.js
+++ src/components/Draw.js
@@ -1,36 +1,58 @@
+import { useEffect, useRef, useState} from "react";
+
 const Draw = (props) => {
+    const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+    const [isDrawing,setIsDrawing] = useState(false);
+    useEffect(
+        () => {
+            const canvas = canvasRef.current;
+            canvas.width = window.innerWidth*2;
+            canvas.height = window.innerHeight*2;
+            canvas.style.width = `${window.innerWidth}px`;
+            canvas.style.height = `${window.innerHeight}px`;
+            const context = canvas.getContext("2d");
+            context.scale(2,2);
+            context.lineWidth = 5;
+            context.lineCap = "round";
+            context.strokeStyle = "black";
+            contextRef.current = context;           
+        }, []
+    )
+    const startDrawing = ({nativeEvent}) => {
+        const {offsetX, offsetY} = nativeEvent;
+      contextRef.current.beginPath();
+      contextRef.current.moveTo(offsetX,offsetY);
+      setIsDrawing(true);
+    }
+
+    const finishDrawing = () => {
+        contextRef.current.closePath();
+        setIsDrawing(false);
+    }
+
+    const draw = ({nativeEvent}) => {
+        const { offsetX, offsetY } = nativeEvent;      
+        if (!isDrawing) {
+            return
+        };
+      contextRef.current.lineTo(offsetX, offsetY);
+      contextRef.current.stroke();
+    }
+
     return (
+
         <div>
             <h1>Draw</h1>
             <hr></hr>
-            <canvas id="canvas"></canvas>
+            <canvas id="canvas"
+            onMouseDown={startDrawing}
+            onMouseUp={finishDrawing}
+            onMouseMove={draw}
+            ref = {canvasRef}
+            ></canvas>
         </div>
     )
 }
-
-// const canvas = document.querySelector("#canvas");
-// const ctx = canvas.getContext("2d");
-// canvas.height = 800;
-// canvas.width = 800;
-// let painting = false;
-// function startPosition(e){
-//   painting = true;
-//   ctx.beginPath();
-// }
-// function finishedPosition(){
-//   painting = false;
-//   ctx.closePath();
-// }
-// function draw(e){
-//   if(!painting) return;
-//   ctx.lineWidth = 2;
-//   ctx.lineCap = "round";
-//   ctx.lineTo(e.clientX, e.clientY);
-//   ctx.stroke();
-// }
-// canvas.addEventListener("mousedown", startPosition);
-// canvas.addEventListener("mouseup", finishedPosition);
-// canvas.addEventListener("mousemove", draw);    
-
 
 export default Draw
